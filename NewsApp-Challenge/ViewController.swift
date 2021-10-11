904925689b2258d83a91db81a6e4c9c9d80954c5
Ercan Garip <ercngp@gmail.com>
2021-10-11 12:27:08 +0300

    Initial Commit

---
+++ NewsApp-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp-Challenge
+//
+//  Created by Ercan Garip on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
